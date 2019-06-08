5a7e1ef8776a9900640564ae54c89e101831028e
JD Long <jal@renre.com>
2019-06-08 14:10:33 -0400

    localhost

--- drivetimegetter.R
+++ drivetimegetter.R
@@ -3,7 +3,7 @@ library(RSelenium)
 
 ## run this after initialization on docker
 
-remDr <- remoteDriver(remoteServerAddr = "192.168.99.100",port = 4445L)
+remDr <- remoteDriver(remoteServerAddr = "localhost",port = 4445L)
 remDr$open()
 
 remDr$navigate("https://www.google.com/maps/dir/8109+Jeffrey+Ct,+Fairfax+Station,+VA+22039/6003+Goethals+Rd,+Fort+Belvoir,+VA+22060/@38.7311954,-77.2367013,13z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x89b653b765e4f829:0x636c45f69657c46b!2m2!1d-77.2595979!2d38.7363011!1m5!1m1!1s0x89b7ac5e4e44b06f:0x2512db922ee9f3d1!2m2!1d-77.1462428!2d38.714176!3e0")
@@ -24,3 +24,4 @@ data2 =
 ## where I can make confidence intervals.  I also plan on extending
 ## this to some other drive locations too -- such as the pentagon and
 ## my favorite bar afterwork and my kids school :)
+
