1193f78812eb31047f7f8dca86b34f7a5e9ab88c
dusty-turner <dusty.s.turner@gmail.com>
2019-06-08 20:14:45 -0400

    google sheets almost completely there

--- functionforgetter.R
+++ functionforgetter.R
@@ -10,8 +10,10 @@ library(RSelenium)
 
 # here = "center point, texas"
 # there = "west point, ny"
-here = "8109 Jeffrey Ct, Fairfax Station, VA 22039"
-there = "6003 Goethals Rd, Fort Belvoir, VA 22060"
+# here = "8109 Jeffrey Ct, Fairfax Station, VA 22039"
+# there = "6003 Goethals Rd, Fort Belvoir, VA 22060"
+here = "1400 west woodstone drive appleton, wi"
+there = "west point, ny"
 
 # remDr <- remoteDriver(port = 4445L)
 remDr <- remoteDriver(remoteServerAddr = "192.168.99.100",port = 4445L)
@@ -25,16 +27,16 @@ timefunc = function(from, to) {
   # locations = tibble::tibble(from = from, to = to)
   remDr$navigate("https://www.google.com/maps/dir///@41.3725056,-73.9712484,15z/data=!4m2!4m1!3e0")
   remDr$screenshot(display = TRUE)
-  Sys.sleep(rlnorm(10,0,1))
+  Sys.sleep(rlnorm(1,2,1))
   webElem <- remDr$findElement(using = 'id', value = "sb_ifc50")
   webElem$sendKeysToElement(list(from, "\uE007"))
   remDr$screenshot(display = TRUE)
-  Sys.sleep(rlnorm(10,0,1))
+  Sys.sleep(rlnorm(1,2,1))
   webElem2 <- remDr$findElement(using = 'id', value = "sb_ifc51")
   webElem2$sendKeysToElement(list(to, "\uE007"))
   remDr$screenshot(display = TRUE)
 
-  Sys.sleep(rlnorm(10,0,1))
+  Sys.sleep(rnorm(1,10,1))
   data <- 
     xml2::read_html(remDr$getPageSource()[[1]]) %>%
     rvest::html_nodes("#section-directions-trip-0 .delay-light span") 
@@ -51,3 +53,5 @@ timefunc = function(from, to) {
 time = timefunc(from = here, to = there)
 
 txtony = timefunc(from ="center point, texas", to = "west point, ny")
+witony = timefunc(from ="1400 west woodstone drive appleton, wi", to = "west point, ny")
+stofl = timefunc(from ="seattle, wa", to = "tampa bay, fl")
--- writingtogooglesheets.R
+++ writingtogooglesheets.R
@@ -1,7 +1,19 @@
 library(googlesheets)
+library(tidyverse)
+
+start = gs_title("drivetimes")
+
+# df = 
+# start %>%
+#   gs_read()
+
+vecdf = tibble(From = from,To = to,Time = unlist(time))
+
+start %>%
+gs_add_row(input = vecdf, verbose = TRUE)
 
 gd_token()
 
-gs_auth("4/ZAGv7ogdwyDYDHeJqPj4_TqvcSO57BRW1-hor75fNsiZXWyLTXnr4pw")
+# gs_auth("4/ZAGv7ogdwyDYDHeJqPj4_TqvcSO57BRW1-hor75fNsiZXWyLTXnr4pw")
 
-gs_title("drivetimes")
+
