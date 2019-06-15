3444e4092f677b7516abeed63627977f1290a71c
JD Long <jal@renre.com>
2019-06-15 17:26:40 -0400

    added csv write

--- google_maps_api_example.R
+++ google_maps_api_example.R
@@ -6,9 +6,18 @@ set.api.key(Sys.getenv("GOOG_MAPS_API"))
 
 ## start with a csv of to/from address pairs ----
 ## using an example pair for testing ---
-to_address <- "215+N+Jefferson+st.+clinton+ky+42031"
-from_address <- "5100+monument+ave+richmond+va+23230"
+to_address <- "215+N+Jefferson+st.+clinton,+ky+42031"
+from_address <- "5100+monument+ave+richmond,+va+23230"
 
 gmapsdistance(origin = from_address,
               destination = to_address,
-              mode = "driving")
+              mode = "driving") %>%
+  as.data.frame %>%
+  mutate(from = from_address, 
+         to = to_address,
+         run_date = Sys.time()) %>%
+  select(from, to, run_date, Time, Distance, Status) -> 
+  drive_time
+
+drive_time %>%
+  write_csv("drive_times.csv", append=TRUE)
