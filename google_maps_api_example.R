da4991b24f5c746d877ba7fc36d60e0e7f58aef2
JD Long <jal@renre.com>
2019-06-17 12:39:40 -0400

    automatic run on: 2019-06-17 12:39

--- google_maps_api_example.R
+++ google_maps_api_example.R
@@ -1,38 +1,54 @@
 library(tidyverse)
 library(gmapsdistance)
 
+## function takes a two addresses and updates the output csv with drive times ----
+
+
+get_drive_times(from_address, to_address) {
+  ## using an example pair for testing ---
+  # to_address <- "215+N+Jefferson+st.+clinton,+ky+42031"
+  # from_address <- "5100+monument+ave+richmond,+va+23230"
+
+    gmapsdistance(
+    origin = from_address,
+    destination = to_address,
+    mode = "driving",
+    traffic_model = "best_guess"
+  ) %>%
+    as.data.frame %>%
+    mutate(from = from_address,
+           to = to_address,
+           run_date = Sys.time()) %>%
+    select(from, to, run_date, Time, Distance, Status) ->
+    drive_time
+  
+  drive_time %>%
+    write_csv("drive_times.csv", append = TRUE)
+  
+  # push this all to github automagically!!! ----
+  
+  cred <- git2r::cred_env("GITHUB_UID", "GITHUB_PAT")
+  git2r::pull(credentials = cred)
+  git2r::commit(message = paste("automatic run on:",
+                                format(Sys.time(), '%Y-%m-%d %H:%M')),
+                all = TRUE)
+  
+  git2r::push(credentials = cred)
+  
+}
+
 ## gmapsdistance key register
 set.api.key(Sys.getenv("GOOG_MAPS_API"))
 
-## start with a csv of to/from address pairs ----
-## using an example pair for testing ---
-to_address <- "215+N+Jefferson+st.+clinton,+ky+42031"
-from_address <- "5100+monument+ave+richmond,+va+23230"
+### read in the pairs of addresses from CSV  -----
+addresses <- read_csv("addresses.csv")
 
-gmapsdistance(origin = from_address,
-              destination = to_address,
-              mode = "driving", 
-              traffic_model = "best_guess") %>%
-  as.data.frame %>%
-  mutate(from = from_address, 
-         to = to_address,
-         run_date = Sys.time()) %>%
-  select(from, to, run_date, Time, Distance, Status) -> 
-  drive_time
-
-drive_time %>%
-  write_csv("drive_times.csv", append=TRUE)
-
-# push this all to github automagically!!! ----
-
-cred <- git2r::cred_env("GITHUB_UID", "GITHUB_PAT")
-git2r::pull(credentials = cred)
-git2r::commit(message = paste(
-  "automatic run on:",
-  format(Sys.time(), '%Y-%m-%d %H:%M')
-),
-all = TRUE)
-
-git2r::push(credentials = cred)
+for (i in 1:nrow(addresses)) {
+  # don't tell Jenny Bryan that sometimes I find loops easier to grok
+  
+  get_drive_times(addresses[i, ]$to_address, addresses[i, ]$from_address)
+  print(paste("finished row:", i))
+  
+}
 
 
